359f12c6940b3c3f389c3e816390edfa3eb583ce
chris haynes <chrishaynes@chriss-mbp.home>
2019-05-31 18:45:16 -0400

    connection to mysql established

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Wilmajo35$",
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    
+  });
