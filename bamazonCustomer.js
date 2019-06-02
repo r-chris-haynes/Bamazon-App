555026d4c955783f390dd8e2fa58203bbc47707e
chris haynes <chrishaynes@chriss-mbp.home>
2019-06-02 14:27:17 -0400

    Table displayed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require('cli-table');
+
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -12,5 +14,30 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    
+    showItems();
   });
+
+//   ===========================================================
+
+function showItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if(err) throw err;
+
+        var table = new Table({
+            head: ["Item Id", "Product Name", "Price", "Quantity"],
+            colWidths: [10, 20, 10, 10]
+        });
+        for (let i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]
+            );
+        };
+        
+        console.log(table.toString());
+    });
+    setTimeout(purchase, 1000); 
+};
+
+
+
+  
