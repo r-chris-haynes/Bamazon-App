edf808d996fd778f204d6eff4944ecddd3a5dcf2
chris haynes <chrishaynes@chriss-mbp.home>
2019-06-04 15:43:09 -0400

    updated validate function to include item_ids not in the ids array

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    // console.log("connected as id " + connection.threadId + "\n");
     showItems();
 });
 
@@ -23,11 +22,7 @@ function showItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
-        var ids = [];
-        for(let i = 0; i < res.length; i++) {
-            ids.push(res[i].item_id);
-        }
-        var table = new Table({
+       var table = new Table({
             head: ["Item Id", "Product Name", "Price", "Quantity"],
             colWidths: [10, 20, 10, 10]
         });
@@ -37,15 +32,21 @@ function showItems() {
                 [res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]
             );
         };
-
         console.log(table.toString());
-        console.log(ids);
     });
     
     setTimeout(purchase, 1000);
 };
 
 function purchase() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        var ids = [];
+        for(let i = 0; i < res.length; i++) {
+            ids.push(res[i].item_id);
+        }
+        // console.log(ids);
     inquirer.prompt([
         {
             name: "buy",
@@ -53,11 +54,12 @@ function purchase() {
             message: "Please enter the id of the item you would like to purchase.",
             
             validate: function (value) {
-                if (isNaN(value) == false) {
+                if (isNaN(value) == false && value <= ids.length) {
                     return true;
                 } else {
                     // return false;
                     console.log(" Invalid entry. Please try again.")
+                    // console.log(ids);
                 }
             }
         },
@@ -75,17 +77,19 @@ function purchase() {
             }
         }
     ])
-        .then(function (answer) {
-            // console.log(answer);
-            if (answer.buy > 10) {
-                console.log("That item doesn't exist. Please try again.");
-                purchase();
-            } else {
+        // .then(function (answer) {
+        //     // console.log(answer);
+        //     if (answer.buy > ids.length) {
+        //         console.log("That item doesn't exist. Please try again.");
+        //         purchase();
+        //     } else {
                 
-            }
+        //     }
 
-        });
+        // });
 }
 
 
 
+
+    )}
