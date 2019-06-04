7457cfa7d50ea5970b2fb47e1e2a959a46669e02
chris haynes <chrishaynes@chriss-mbp.home>
2019-06-04 14:21:01 -0400

    created var array to store item_ids

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,78 +4,88 @@ var Table = require('cli-table');
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Wilmajo35$",
-  database: "bamazon_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Wilmajo35$",
+    database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     // console.log("connected as id " + connection.threadId + "\n");
     showItems();
-  });
+});
 
 //   ===========================================================
 
 function showItems() {
     connection.query("SELECT * FROM products", function (err, res) {
-        if(err) throw err;
+        if (err) throw err;
 
+        var ids = [];
+        for(let i = 0; i < res.length; i++) {
+            ids.push(res[i].item_id);
+        }
         var table = new Table({
             head: ["Item Id", "Product Name", "Price", "Quantity"],
             colWidths: [10, 20, 10, 10]
         });
+        
         for (let i = 0; i < res.length; i++) {
             table.push(
                 [res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]
             );
         };
-        
+
         console.log(table.toString());
+        console.log(ids);
     });
-    setTimeout(purchase, 1000); 
+    
+    setTimeout(purchase, 1000);
 };
 
 function purchase() {
     inquirer.prompt([
-            {
-                name: "buy",
-                type: "input",
-                message: "Please enter the id of the item you would like to purchase.",
-                validate: function(value) {
-                    if (isNaN(value) == false) {
-                        return true;
-                    } else {
-                        // return false;
-                        console.log(" Invalid entry. Please try again.")
-                    }
+        {
+            name: "buy",
+            type: "input",
+            message: "Please enter the id of the item you would like to purchase.",
+            
+            validate: function (value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    // return false;
+                    console.log(" Invalid entry. Please try again.")
                 }
-            },
-            // {
-            //     name: "amount",
-            //     type: "input",
-            //     message: "How many would you like to purchase?",
-            //     validate: function(value) {
-            //         if (isNaN(value) == false) {
-            //             return true;
-            //         } else {
-            //             // return false;
-            //             console.log(" Invalid entry. Please try again.")
-            //         }
-            //     }
-            // }
-        ])
-        .then(function(answer){
-            console.log(answer);
+            }
+        },
+        {
+            name: "amount",
+            type: "input",
+            message: "How many would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    // return false;
+                    console.log(" Invalid entry. Please try again.")
+                }
+            }
+        }
+    ])
+        .then(function (answer) {
+            // console.log(answer);
             if (answer.buy > 10) {
                 console.log("That item doesn't exist. Please try again.");
                 purchase();
+            } else {
+                
             }
-            
-        })
-        
+
+        });
 }
 
-  
+
+
